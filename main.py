463c88f6fe3bedef0dfed7976caf60d9c22c19fc
Caio Carvalho <caiocarvalho.py@gmail.com>
2020-11-26 16:57:11 -0400

    Validando CPNJ e construindo uma Factory

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from cpf import Cpf
+from cpf_cnpj import Documento
+# from validate_docbr import CNPJ
 
-cpf_um = Cpf('15316264754')
-print(cpf_um)
+# cpf_um = Cpf('15316264754')
+# print(cpf_um)
+
+# exemplo_cnpj = '35379838000112'
+exemplo_cpf = '32007832062'
+# cnpj = CNPJ()
+# print(cnpj.validate(exemplo_cnpj))
+documento = Documento.cria_documento(exemplo_cpf)
+print(documento)
