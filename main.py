37a14e36d6836ae1881d2fd7b0e654a7ccbbac24
Caio Carvalho <caiocarvalho.py@gmail.com>
2020-11-30 18:31:06 -0400

    Manipulando e formatando datas

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from telefonesBr import TelefonesBr
-import re
+from datetime import datetime, timedelta
+from datasBr import DatasBr
 
-telefone = '550012345678'
-
-telefone_objeto = TelefonesBr(telefone)
-# padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
-# resposta = re.search(padrao, telefone)
-# print(resposta.group())
-
-print(telefone_objeto)
+hoje = DatasBr()
+print(hoje.tempo_cadastro())
