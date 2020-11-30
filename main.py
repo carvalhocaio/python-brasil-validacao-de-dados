3dfd34a0e20e55d993f67dcdab7faa155d4dacc0
Caio Carvalho <caiocarvalho.py@gmail.com>
2020-11-30 11:21:03 -0400

    Validando telefones com expressões regulares

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from cpf_cnpj import Documento
-# from validate_docbr import CNPJ
+from telefonesBr import TelefonesBr
+import re
 
-# cpf_um = Cpf('15316264754')
-# print(cpf_um)
+telefone = '550012345678'
 
-# exemplo_cnpj = '35379838000112'
-exemplo_cpf = '32007832062'
-# cnpj = CNPJ()
-# print(cnpj.validate(exemplo_cnpj))
-documento = Documento.cria_documento(exemplo_cpf)
-print(documento)
+telefone_objeto = TelefonesBr(telefone)
+# padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+# resposta = re.search(padrao, telefone)
+# print(resposta.group())
+
+print(telefone_objeto)
